f8b5cac632a6b272b5f0a074f9976d5668ada8af
Matt Stanford <mds6058@gmail.com>
2018-03-01 19:09:32 -0500

    Added uitableview extension for flexible header

---
+++ Hacker News Zero/Extensions/UITableView+FlexibleHeader.swift
@@ -0,0 +1,39 @@
+//
+//  UITableView+FlexibleHeader.swift
+//  Hacker News Zero
+//
+//  Created by Matt Stanford on 3/1/18.
+//  Copyright © 2018 locacha. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func layoutTableHeaderView() {
+        
+        guard let headerView = self.tableHeaderView else { return }
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let headerWidth = headerView.bounds.size.width;
+        let temporaryWidthConstraints = NSLayoutConstraint.constraints(withVisualFormat: "[headerView(width)]", options: NSLayoutFormatOptions(rawValue: UInt(0)), metrics: ["width": headerWidth], views: ["headerView": headerView])
+        
+        headerView.addConstraints(temporaryWidthConstraints)
+        
+        headerView.setNeedsLayout()
+        headerView.layoutIfNeeded()
+        
+        let headerSize = headerView.systemLayoutSizeFitting(UILayoutFittingCompressedSize)
+        let height = headerSize.height
+        var frame = headerView.frame
+        
+        frame.size.height = height
+        headerView.frame = frame
+        
+        self.tableHeaderView = headerView
+        
+        headerView.removeConstraints(temporaryWidthConstraints)
+        headerView.translatesAutoresizingMaskIntoConstraints = true
+        
+    }
+}
--- Hacker News Zero/View/Article Detail/CommentsViewController.swift
+++ Hacker News Zero/View/Article Detail/CommentsViewController.swift
@@ -13,17 +13,45 @@ class CommentsViewController: UIViewController, ArticleViewable {
     var navigator: ArticleNavigator?
     var currentShowingArticle: Article?
     
+    @IBOutlet weak private var titleLabel: UILabel!
+    @IBOutlet weak private var infoLabel: UILabel!
+    
+    @IBOutlet weak private var tableView: UITableView!
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         showCurrentArticle()
+   
     }
-
+    
     //MARK: ArticleViewable protocol
     
     func gotNewArticle() {
-        //TODO
+        
+        titleLabel.text = navigator?.currentArticle?.title
+        infoLabel.text = "Info stuff"
+        
+        tableView.layoutTableHeaderView()
     }
-   
+}
 
+extension CommentsViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TestCell", for: indexPath)
+        
+    
+        
+        return cell
+    }
 }
