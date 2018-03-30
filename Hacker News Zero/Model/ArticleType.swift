027d9014e71560373dfdad5f92f05859207dc2ee
Matt Stanford <mds6058@gmail.com>
2018-03-29 21:49:22 -0400

    Preliminary options vc

---
+++ Hacker News Zero/Model/ArticleType.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleType.swift
+//  Hacker News Zero
+//
+//  Created by Matt Stanford on 3/28/18.
+//  Copyright © 2018 locacha. All rights reserved.
+//
+
+import Foundation
+
+enum ArticleType {
+    case frontpage
+    case askhn
+    case showhn
+    case jobs
+    case new
+}
---
+++ Hacker News Zero/View/Options/OptionsViewController.swift
@@ -0,0 +1,63 @@
+//
+//  OptionsViewController.swift
+//  Hacker News Zero
+//
+//  Created by Matt Stanford on 3/28/18.
+//  Copyright © 2018 locacha. All rights reserved.
+//
+
+import UIKit
+
+class OptionsViewController: UIViewController {
+    
+    @IBOutlet weak private var tableView: UITableView!
+    
+    var articleTypeSelections: [ArticleType] = [.frontpage, .askhn, .showhn, .jobs, .new]
+    
+
+}
+
+extension OptionsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return articleTypeSelections.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTypeCell", for: indexPath)
+        
+        let articleType = articleTypeSelections[indexPath.row]
+        var cellText = ""
+        
+        switch articleType {
+        case .frontpage:
+            cellText = "Front Page"
+        case .askhn:
+            cellText = "Ask HN"
+        case .showhn:
+            cellText = "Show HN"
+        case .jobs:
+            cellText = "Jobs"
+        case .new:
+            cellText = "New"
+        }
+        
+        cell.textLabel?.text = cellText
+        
+        return cell
+    }
+    
+    //MARK: UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        
+    }
+    
+}
