693d8476ff18857a44b6929148f97253878d7e0d
Matt Stanford <mds6058@gmail.com>
2018-07-04 16:41:14 -0400

    Added progressview to webview

--- Hacker News Zero/View/Article Detail/WebViewController.swift
+++ Hacker News Zero/View/Article Detail/WebViewController.swift
@@ -11,19 +11,42 @@ import WebKit
 
 class WebViewController: UIViewController, ArticleViewable {
     @IBOutlet private weak var webView: WKWebView!
+    @IBOutlet private weak var progressView: UIProgressView!
     
     var viewModel = WebViewModel()
+    
+    var progressObserver: NSKeyValueObservation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         webView.navigationDelegate = self
+        setupProgressBar()
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
    
         showNewArticleIfNecessary()
+    }
+    
+    func setupProgressBar() {
+        
+        self.progressObserver = webView.observe(\.estimatedProgress) { [weak self] (webView, _) in
+            self?.progressView.setProgress(Float(webView.estimatedProgress), animated: true)
+            
+            if webView.estimatedProgress >= 1 {
+                
+                UIView.animate(withDuration: 0.3, delay: 0.3, options: UIViewAnimationOptions.curveEaseOut, animations: {
+                    self?.progressView.alpha = 0
+                }, completion: { _ in
+                    self?.progressView.setProgress(0, animated: false)
+                })
+            } else {
+                self?.progressView.alpha = 1
+            }
+        }
+        
     }
     
     func showNewArticleIfNecessary() {
