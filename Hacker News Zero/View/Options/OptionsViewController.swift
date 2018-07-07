5f5902a29af309c2803aa6b530815d6d00dfc151
Matt Stanford <mds6058@gmail.com>
2018-07-07 14:49:10 -0400

    Added other sections to side menu

--- Hacker News Zero/View/Options/OptionsViewController.swift
+++ Hacker News Zero/View/Options/OptionsViewController.swift
@@ -12,6 +12,51 @@ protocol OptionsDelegate: class {
     func refreshArticles(type: ArticleType)
 }
 
+enum OptionSection {
+    case main
+    case themes
+    case socialmedia
+    
+    var title: String? {
+        switch self {
+        case .main:
+            return nil
+        case .themes:
+            return "Themes"
+        case .socialmedia:
+            return "Social Media"
+        }
+    }
+}
+
+enum ThemeSelection: String {
+    case classic = "HN Zero Classic"
+    case dark = "Dark Mode"
+    
+    var associatedTheme: ColorScheme {
+        switch self {
+        case .classic:
+            return ColorScheme.standard
+        case .dark:
+            return ColorScheme.dark
+        }
+    }
+}
+
+enum SocialMediaSelection: String {
+    case facebook = "Facebook"
+    case twitter = "Twitter"
+    
+    var associatedUrl: String {
+        switch self {
+        case .facebook:
+            return "https://www.facebook.com/1080017705457038"
+        case .twitter:
+            return "https://twitter.com/hackernewszero"
+        }
+    }
+}
+
 class OptionsViewController: UIViewController {
     
     @IBOutlet weak private var tableView: UITableView!
@@ -23,7 +68,10 @@ class OptionsViewController: UIViewController {
     
     var colorScheme: ColorScheme = HackerNewsRepository.shared.settingsCache.colorScheme
     
+    var sections: [OptionSection] = [.main, .themes, .socialmedia]
     var articleTypeSelections: [ArticleType] = [.frontpage, .askhn, .showhn, .jobs, .new]
+    var themeSelections: [ThemeSelection] = [.classic, .dark]
+    var socialMediaSelections: [SocialMediaSelection] = [.facebook, .twitter]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,20 +91,60 @@ extension OptionsViewController: ColorChangeable {
 extension OptionsViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard section < sections.count else {
+            return nil
+        }
+        let section = sections[section]
+        
+        return section.title
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return articleTypeSelections.count
+        guard section < sections.count else {
+            return 0
+        }
+        let section = sections[section]
+        
+        switch section {
+        case .main:
+            return articleTypeSelections.count
+        case .themes:
+            return themeSelections.count
+        case .socialmedia:
+            return socialMediaSelections.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell: UITableViewCell
+        let section = sections[indexPath.section]
+        
+        switch section {
+        case .main:
+            cell = articleCell(tableView, indexPath: indexPath)
+        case .themes:
+            cell = themeCell(tableView, indexPath: indexPath)
+        case .socialmedia:
+            cell = socialMediaCell(tableView, indexPath: indexPath)
+        }
+        
+        cell.backgroundColor = colorScheme.contentBackgroundColor
+        cell.textLabel?.textColor = colorScheme.contentTextColor
+        return cell
+    }
+    
+    private func articleCell(_ tableView: UITableView, indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTypeCell", for: indexPath)
-        
+
         let articleType = articleTypeSelections[indexPath.row]
-        var cellText = ""
         
+        let cellText: String
         switch articleType {
         case .frontpage:
             cellText = "Front Page"
@@ -71,8 +159,24 @@ extension OptionsViewController: UITableViewDelegate, UITableViewDataSource {
         }
         
         cell.textLabel?.text = cellText
-        cell.backgroundColor = colorScheme.contentBackgroundColor
-        cell.textLabel?.textColor = colorScheme.contentTextColor
+    
+        return cell
+    }
+    
+    private func themeCell(_ tableView: UITableView, indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTypeCell", for: indexPath)
+        
+        let themeSelection = themeSelections[indexPath.row]
+        cell.textLabel?.text = themeSelection.rawValue
+        
+        return cell
+    }
+    
+    private func socialMediaCell(_ tableView: UITableView, indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTypeCell", for: indexPath)
+        
+        let selection = socialMediaSelections[indexPath.row]
+        cell.textLabel?.text = selection.rawValue
         
         return cell
     }
